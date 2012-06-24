e4c1eb3a3c792edc08fcd2b347dac5ae32bb5796
Paul Anderson <paul@andersonloco.com>
2012-06-24 19:29:33 -0400

    Put quotes around bork, added output.

--- tiefile.pl
+++ tiefile.pl
@@ -3,11 +3,15 @@
 use diagnostics;
 use Tie::File;
 
-tie @array, 'Tie::File', bork or die "She is angry, $!";
+tie @array, 'Tie::File', "bork" or die "She is angry, $!";
 
 for ($i=0;$i<20;$i++) {
     $array[$i]=$i;
 }
+
+print $array[9] . "\n";
+
+
 
 untie @array;
 
