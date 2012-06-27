7371bf5b09c3838d1d31133cb16d774cc4174a5e
Paul Anderson <paul@andersonloco.com>
2012-06-26 20:44:10 -0400

    Now outputs valid YAML.

--- pwstuff-yaml.pl
+++ pwstuff-yaml.pl
@@ -1,20 +1,43 @@
 #!/usr/bin/perl
 
 use diagnostics;
+use YAML::XS;
+#use Data::Dumper;
 
-open(PWFILE, "glurp");
-open(OUTFILE, ">output");
+open( PWFILE, "glurp" );
 
-chomp(@entries=<PWFILE>);
+#open(OUTFILE, ">output");
+
+chomp( @entries = <PWFILE> );
+
+#%data{}{}= ( ); #Empty hash of hashes
 
 foreach (@entries) {
-  #print $_;
-  ($user,$pw,$uid,$gid,$gecos,$home,$shell) = split(':', $_);
-  if ($user=~/#/) {
-    next;
-  }
-  print "Name: $user \n";
-  print OUTFILE join(':',$user,$shell);
+
+    #print $_;
+    ( $user, $pw, $uid, $gid, $gecos, $home, $shell ) = split( ':', $_ );
+    if ( $user =~ /#/ ) {    #Eat comment lines.
+        next;
+    }
+
+    %data = (
+        $user => {
+            "pw"    => $pw,
+            "uid"   => $uid,
+            "gid"   => $gid,
+            "gecos" => $gecos,
+            "home"  => $home,
+            "shell" => $shell
+        }
+    );    #Add to hash.
+
+    #print "Name: $user \n";
+    print Dump(\%data);
+    #  print OUTFILE join(':',$user,$shell);
 }
 
-close OUTFILE;
+#close OUTFILE;
+
+#print Dump( \%data );
+
+#print Dumper(\%data);
