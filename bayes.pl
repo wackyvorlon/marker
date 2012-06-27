800c59e42a526db208832fe93ccfb8392eef1018
Paul Anderson <paul@andersonloco.com>
2012-06-27 13:29:31 -0400

    Reads in file, generates weighting hash. Next step is feeding this into the bayes algorithm.

--- bayes.pl
+++ bayes.pl
@@ -5,6 +5,17 @@
 use diagnostics;
 use Algorithm::Naivebayes;
 use Data::Dumper;
+use Lingua::EN::Splitter qw(words);
+use Lingua::EN::StopWords qw(%StopWords);
+use File::Slurp;
+
+sub invert_string {
+  #Takes string input, creates hash of words weighted by count.
+  my ($string,$weight,$hash) = @_;
+  $hash->{$_} += $weight for
+    grep {!$StopWords{$_}}
+      @{words(lc($string))};
+}
 
 my $nb = Algorithm::NaiveBayes->new;
 
@@ -17,4 +28,12 @@ $nb->train;
 my $result = $nb->predict(attributes => {bar => 3, blurp => 2});
 
 print Dumper(\$result);
+
+$text=read_file('input.txt');
+
+my %valhash;
+
+invert_string($text, 1, \%valhash);  #Generate hash of words with word count.
+
+print Dumper(\%valhash);
 
