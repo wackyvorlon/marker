0bb1d6b21326abb1a93c09a250083c6fcbc24b72
Paul Anderson <paul@andersonloco.com>
2012-06-27 10:47:00 -0400

    Playing with Term::Activity

---
+++ termact.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use diagnostics;
+use Term::Activity;
+
+$ta = new Term::Activity;
+
+#$foo=100;
+
+#while () {
+#  $ta->tick;
+#  sleep 0.5;
+#  $foo--;
+#}
+
+for ($i=0;$i<100;$i++) {
+  $ta->tick;
+  sleep 1;
+}
