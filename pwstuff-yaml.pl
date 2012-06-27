cfad97aac6ce3e764ea05eb8bd02390217a132e8
Paul Anderson <paul@andersonloco.com>
2012-06-26 20:14:34 -0400

    Adding YAML output support.

---
+++ pwstuff-yaml.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use diagnostics;
+
+open(PWFILE, "glurp");
+open(OUTFILE, ">output");
+
+chomp(@entries=<PWFILE>);
+
+foreach (@entries) {
+  #print $_;
+  ($user,$pw,$uid,$gid,$gecos,$home,$shell) = split(':', $_);
+  if ($user=~/#/) {
+    next;
+  }
+  print "Name: $user \n";
+  print OUTFILE join(':',$user,$shell);
+}
+
+close OUTFILE;
