aa10d0aadd4dc66b9637e0560951644633a73c25
Paul Anderson <paul@andersonloco.com>
2012-06-30 13:25:49 -0400

    Cache setup.

--- marker.pl
+++ marker.pl
@@ -2,21 +2,33 @@
 
 #Load modules, use MD5 hash to identify already seen documents. Result of processing stored in a hash tied to a file.
 
-use Cache::Cache;   #Handles caching.
+use CHI;   #Handles caching.
 use Digest::MD5;    #MD5 hash determines if we've seen this doc before.
 use Text::Markdown; #Convert markdown to HTML
 use File::Slurp;    #Generic functions for reading in files.
 use diagnostics;    #Gives more informative errors.
 
-my %seen;
+
+#Caching setup
+
+my $cache = CHI->new( driver => 'File',
+		      root_dir => './'
+		    );
+
+
 
 #open(INFILE, $ARGV[0]) or die $!;
 
 $data = read_file($ARGV[0]);
 
-$digest = md5($data);
+$digest = Digest::MD5::md5($data);
 
-if (defined %seen{$digest}) {
+$result = $cache->get($digest);
+
+if (defined $result) {
   print "Seen this before\n";
 }
+
+$cache->set($digest, $data, "20 min");
+
 
