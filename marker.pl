0f17546d6a0d5222666ad1a23bf44e65c3d33fac
Paul Anderson <paul@andersonloco.com>
2012-06-30 13:00:02 -0400

    Added modules.

--- marker.pl
+++ marker.pl
@@ -1,2 +1,9 @@
 #!/usr/bin/perl
 
+#Load modules, use MD5 hash to identify already seen documents. Result of processing stored in a hash tied to a file.
+
+use Digest::MD5;
+use Tie::File;
+use Text::Markdown;
+
+
