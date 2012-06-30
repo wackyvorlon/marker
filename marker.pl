4b7ec726b3b0ee624628762da2928205051923b2
Paul Anderson <paul@andersonloco.com>
2012-06-30 13:05:37 -0400

    Load file and see if it's been in the cache.

--- marker.pl
+++ marker.pl
@@ -3,7 +3,20 @@
 #Load modules, use MD5 hash to identify already seen documents. Result of processing stored in a hash tied to a file.
 
 use Digest::MD5;
-use Tie::File;
+#use Tie::Hash;
 use Text::Markdown;
+use File::Slurp;
+use diagnostics;
 
+my %seen;
+
+#open(INFILE, $ARGV[0]) or die $!;
+
+$data = read_file($ARGV[0]);
+
+$digest = md5($data);
+
+if (defined %seen{$digest}) {
+  print "Seen this before\n";
+}
 
