f4933478ff44f705e3318a5564065dfaef32ea32
Paul Anderson <paul@andersonloco.com>
2012-06-30 14:13:37 -0400

    CHanged to checking timestamp.

--- marker.pl
+++ marker.pl
@@ -6,7 +6,7 @@ use CHI;   #Handles caching.
 use Digest::MD5;    #MD5 hash determines if we've seen this doc before.
 use Text::Markdown qw(markdown); #Convert markdown to HTML
 use File::Slurp;    #Generic functions for reading in files.
-use diagnostics;    #Gives more informative errors.
+#use diagnostics;    #Gives more informative errors.
 
 
 #Caching setup
@@ -16,15 +16,25 @@ my $cache = CHI->new( driver => 'File',
 		    );
 
 
+($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
+ $atime,$mtime,$ctime,$blksize,$blocks)
+  = stat($ARGV[0]);
+
 #Grab data
-$data = read_file($ARGV[0]);
+#$data = read_file($ARGV[0]);
 
-$digest = Digest::MD5::md5($data);
+#$digest = Digest::MD5::md5($data);
+
+$key=$mtime.$ARGV[0];
 
 
-my $value = $cache->compute($digest, '5min', sub {
+
+my $value = $cache->compute($key, '5min', sub {
+			      $data=read_file($ARGV[0]);
 			      my $html = markdown($data);
 			      return $html;
 			    });
 
 print $value;
+
+#print $key;
