7a4d0c97f62b63593e0eda73fe048e009dcf6c74
Paul Anderson <paul@andersonloco.com>
2012-07-19 15:12:07 -0400

    Generates numbers without hyphens, as required by Jeff's code.

--- numtester.pl
+++ numtester.pl
@@ -1,10 +1,15 @@
 #!/usr/bin/perl
 
 use Lingua::Any::Numbers qw(:std);
+use diagnostics;
+
 
 #print to_string(45);
 
-for (1..10) {
-    print to_string($_), "\n";
+for (1..100) {
+    $num=to_string($_);
+    $num =~ s/-/ /g;
+    
+    print "$num\n";
     
 }
