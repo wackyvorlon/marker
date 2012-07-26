4bae12047b3f6792f53ea892cf8fac67d1733b45
Paul Anderson <paul@andersonloco.com>
2012-07-26 12:52:40 -0400

    Initial commit of code to play with bogosort.

---
+++ bogosorter.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl -w
+# bogosorter.pl --- Playing with bogosort
+# Author: Paul Anderson <wackyvorlon@paul-andersons-macbook-pro-3.local>
+# Created: 26 Jul 2012
+# Version: 0.01
+
+#use warnings;
+#use strict;
+
+use Data::Dumper;
+use List::Util qw(shuffle);
+use Acme::Sort::Bogosort;
+use Benchmark qw(:all);
+
+
+@foo = ( 1 .. 5 );
+
+@bork=shuffle(@foo);
+
+
+print Dumper(\@bork);
+
+$result=timethis(5,sub {@foo2=bogosort(@bork)});
+
+print Dumper(\@foo2);
+print timestr($result);
+
+
+__END__
+
+=head1 NAME
+
+bogosorter.pl - Describe the usage of script briefly
+
+=head1 SYNOPSIS
+
+bogosorter.pl [options] args
+
+      -opt --long      Option description
+
+=head1 DESCRIPTION
+
+Stub documentation for bogosorter.pl, 
+
+=head1 AUTHOR
+
+Paul Anderson, E<lt>wackyvorlon@paul-andersons-macbook-pro-3.localE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Paul Anderson
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 BUGS
+
+None reported... yet.
+
+=cut
