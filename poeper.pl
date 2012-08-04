2048589cbfd761ead187622dd0cb826d52af7da8
Paul Anderson <paul@andersonloco.com>
2012-08-04 03:04:20 -0400

    Example code entered.

--- poeper.pl
+++ poeper.pl
@@ -5,10 +5,44 @@
 # Version: 0.01
 
 use warnings;
+
 #use strict;
 
 use POE;
 
+sub handler_start {
+    my ( $kernel, $heap, $session ) = @_[ KERNEL, HEAP, SESSION ];
+    print "Session ", $session->ID, " has started.\n";
+    $heap->{count} = 0;
+    $kernel->yield('increment');
+
+}
+
+sub handler_increment {
+    my ( $kernel, $heap, $session ) = @_[ KERNEL, HEAP, SESSION ];
+    print "Session ", $session->ID, " counted to ", ++$heap->{count}, ".\n";
+    $kernel->yield('increment') if $heap->{count} < 10;
+
+}
+
+sub handler_stop {
+    print "Session ", $_[SESSION]->ID, " has stopped.\n";
+
+}
+
+for ( 1 .. 10 ) {
+    POE::Session->create(
+        inline_states => {
+            _start    => \&handler_start,
+            increment => \&handler_increment,
+            _stop     => \&handler_stop,
+        }
+    );
+
+}
+
+POE::Kernel->run();
+exit;
 
 
 __END__
