490f7b8670d6cd3437eaa5ef506f6fe42708fd1d
Paul Anderson <paul@andersonloco.com>
2012-08-04 02:52:29 -0400

    Goofing around with POE.

---
+++ poeper.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/myperl -w
+# poeper.pl --- Playing with POE
+# Author: Paul Anderson <wackyvorlon@paul-andersons-macbook-pro-3.local>
+# Created: 04 Aug 2012
+# Version: 0.01
+
+use warnings;
+#use strict;
+
+use POE;
+
+
+
+__END__
+
+=head1 NAME
+
+poeper.pl - Describe the usage of script briefly
+
+=head1 SYNOPSIS
+
+poeper.pl [options] args
+
+      -opt --long      Option description
+
+=head1 DESCRIPTION
+
+Stub documentation for poeper.pl, 
+
+=head1 AUTHOR
+
+Paul Anderson, E<lt>wackyvorlon@paul-andersons-macbook-pro-3.localE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Paul Anderson
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 BUGS
+
+None reported... yet.
+
+=cut
