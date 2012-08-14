4747e1d3f6639071042af9f8869d725ddf6c1eff
Paul Anderson <paul@andersonloco.com>
2012-08-14 16:57:04 -0400

    BAffled as to why this isn't working. Is dying silently.

--- euler8.pl
+++ euler8.pl
@@ -7,7 +7,7 @@
 use warnings;
 use Data::Dumper;
 use diagnostics;
-use 5.16.0;
+use 5.14.0;
 
 #use strict;
 
@@ -16,18 +16,22 @@ use 5.16.0;
 my $num="7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450";
 
 #$products = $num=~ /(\d)(\d){\d}(\d)(\d)/{$1*$2*$3*$4*$5}/rge;
-my @products;
-my $prev;
+#my @products;
+my $result;
 
 
 while ($num=~/(\d)(\d)(\d)(\d)(\d)/gi) {
         #print $1*$2*$3*$4*$5."\n";
-    #push @products, $1*$2*$3*$4*$5;          # Put the product in our array.
+    $cur = $1*$2*$3*$4*$5;          # Put the product in our array.
     pos $num=(pos $num)-4;                   # Reset position where matching will begin.
                                              # pos $num defaults to the position just
                                              # after the *last* character in our match.
                                              # We want our second match to begin after
                                              # the *first* character.
+    #$cur = $1*$2*$3*$4*$5;
+    
+    $result = $cur<=>$result ? $cur;         # If the curent value is greater than previous, set
+                                           # $result to new value.
     
 }
 
@@ -35,9 +39,8 @@ while ($num=~/(\d)(\d)(\d)(\d)(\d)/gi) {
 
 #my @sortedproducts = sort numerically @products;
 
-#print "Greatest value is $sortedproducts[1].\n";
+print "Greatest value is $result.\n";
 
-print Dumper(\@products);
 
 
 #print $products[1];
