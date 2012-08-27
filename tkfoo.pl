feea40b40f3227b0af1cd1dd447b02c4c127a80e
Paul Anderson <paul@andersonloco.com>
2012-08-26 22:56:44 -0400

    Poaying with Tk

---
+++ tkfoo.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/myperl -w
+# tkfoo.pl --- Playing with Tk
+# Author: Paul Anderson <wackyvorlon@paul-andersons-macbook-pro-3.local>
+# Created: 26 Aug 2012
+# Version: 0.01
+
+use warnings;
+#use strict;
+
+use Tk;
+
+#$mw->
+$mw=MainWindow->new;
+
+$mw->Label(-text=> "Hello,world"))
+
+__END__
+
+=head1 NAME
+
+tkfoo.pl - Describe the usage of script briefly
+
+=head1 SYNOPSIS
+
+tkfoo.pl [options] args
+
+      -opt --long      Option description
+
+=head1 DESCRIPTION
+
+Stub documentation for tkfoo.pl, 
+
+=head1 AUTHOR
+
+Paul Anderson, E<lt>wackyvorlon@paul-andersons-macbook-pro-3.localE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012 by Paul Anderson
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+=head1 BUGS
+
+None reported... yet.
+
+=cut
